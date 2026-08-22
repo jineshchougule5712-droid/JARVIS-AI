de180b8abc1dd7099a9f82b8b1f86ac51473c430
jineshchougule5712-droid <jineshchougule5712@gmail.com>
2026-08-22 19:55:33 +0530

    Delete Jarvis directory

--- Jarvis/backend/src/main/java/com/jinesh/jarvis/JarvisApplication.java
+++
@@ -1,3 +0,0 @@
-package com.jinesh.jarvis;
-import org.springframework.boot.SpringApplication; import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication public class JarvisApplication { public static void main(String[] a){SpringApplication.run(JarvisApplication.class,a);} }
